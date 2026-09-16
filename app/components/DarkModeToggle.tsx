12d469e9f966341400ac38696f70d203471bd7f5
Egor Denisov <designer.egor@ya.ru>
2026-09-16 11:45:50 -0400

    refactor: use local theme icons

--- app/components/DarkModeToggle.tsx
+++ app/components/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import { IconMoon, IconSun } from '@supabase/ui'
+import { IconMoon, IconSun } from '~/components/Icons'
 import { useTheme } from '~/lib/theme'
 
 function DarkModeToggle() {
@@ -14,30 +14,24 @@ function DarkModeToggle() {
 
   return (
     <div className="flex items-center">
-      <IconSun className="text-scale-900" strokeWidth={2} />
+      <IconSun className="text-scale-900" strokeWidth={2} aria-hidden="true" />
       <button
         type="button"
-        aria-pressed="false"
-        className={`
-                relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent 
-                transition-colors duration-200 ease-in-out focus:outline-none ${
-                  isDarkMode ? 'bg-scale-500' : 'bg-scale-900'
-                } mx-5
-              `}
-        onClick={() => toggleDarkMode()}
+        aria-pressed={isDarkMode}
+        aria-label="Toggle dark mode"
+        className={`relative mx-5 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-brand-600 focus:ring-offset-2 ${
+          isDarkMode ? 'bg-scale-500' : 'bg-scale-900'
+        }`}
+        onClick={toggleDarkMode}
       >
-        <span className="sr-only">Toggle Themes</span>
         <span
           aria-hidden="true"
-          className={`
-                  ${
-                    isDarkMode ? 'translate-x-5' : 'translate-x-0'
-                  } inline-block h-5 w-5 transform
-                  rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out dark:bg-scale-300
-                `}
+          className={`${
+            isDarkMode ? 'translate-x-5' : 'translate-x-0'
+          } inline-block h-5 w-5 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out dark:bg-scale-300`}
         />
       </button>
-      <IconMoon className="text-scale-900" strokeWidth={2} />
+      <IconMoon className="text-scale-900" strokeWidth={2} aria-hidden="true" />
     </div>
   )
 }
